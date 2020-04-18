c51b8d6101c68c1e20ef62ad876de9a5dd282055
Mahima <mahima.srivastava@in.ibm.com>
2020-04-18 22:11:26 +0530

    Implementation of Queue Data Structure

--- Swift/Queue.playground/Contents.swift
+++ Swift/Queue.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
+/*
+ This code will show the implementation of queue data structure. It will have following methods:
+    1. Insertion (1,2,3,4) -> Insert 5 -> (1,2,3,4,5)
+    2. Deletion (1,2,3,4,5) -> Delete -> (2,3,4,5)
+    3. Getting the count of elements in queue
+    4. Get all the values
+ 
+ */
+struct Queue<T> {
+    var list: [T] = []
+    
+    mutating func push(val: T) {
+        list.append(val)
+    }
+    
+    mutating func delete() -> T? {
+        return list.remove(at: 0)
+    }
+    
+    mutating func getCountOfElements() -> Int {
+        return list.count
+    }
+    
+    mutating func getValues() -> [T] {
+        return list
+    }
+ }
 
-var str = "Hello, playground"
+var bullet = Queue<Int>()
+
+bullet.push(val: 0)
+bullet.push(val: 1)
+bullet.push(val: 2)
+bullet.push(val: 3)
+print(bullet.getValues())
+print(bullet.getCountOfElements())
+print(bullet.delete() ?? "")
+print(bullet.getValues())
+print(bullet.getCountOfElements())
+
+var strBullet = Queue<String>()
+
+strBullet.push(val: "M")
+strBullet.push(val: "A")
+strBullet.push(val: "H")
+strBullet.push(val: "I")
+strBullet.push(val: "M")
+strBullet.push(val: "A")
+print(strBullet.getValues())
+print(strBullet.getCountOfElements())
+print(strBullet.delete() ?? "")
+print(strBullet.getValues())
+print(strBullet.getCountOfElements())
